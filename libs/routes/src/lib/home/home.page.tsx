54528549d8112c8a5117873f6619e6f33ace78c1
Lee Marsden <leemarsden@lees-mbp-3.home>
2022-06-05 19:56:41 +0100

    Updates to home page links

--- libs/routes/src/lib/home/home.page.tsx
+++ libs/routes/src/lib/home/home.page.tsx
@@ -1,5 +1,19 @@
+import { NavLink } from 'react-router-dom';
+import { Nav } from '../elephant/components';
+
 export const HomePage = () => (
   <main>
     <h2>Home Page</h2>
+    <ul>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/elephant/stampede">Elephant</NavLink>
+      </li>
+      <li>
+        <NavLink to="/drip">Drip</NavLink>
+      </li>
+    </ul>
   </main>
 );
